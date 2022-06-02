793b0c01e6594e66f33bfb4e46790d89530b210a
Nick Crews <nickcrews35@gmail.com>
2022-06-01 21:32:48 -0400

    Renamed files

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "basin-swift",
+    name: "OpenBasin",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "basin-swift",
-            targets: ["basin-swift"]),
+            name: "OpenBasin",
+            targets: ["OpenBasin"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,10 +19,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "basin-swift",
+            name: "OpenBasin",
             dependencies: []),
         .testTarget(
-            name: "basin-swiftTests",
-            dependencies: ["basin-swift"]),
+            name: "OpenBasinTests",
+            dependencies: ["OpenBasin"]),
     ]
 )
